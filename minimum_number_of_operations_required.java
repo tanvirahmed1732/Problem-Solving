4fea12a7f20321cb15fb5f0a45f969fcc2e4a220
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-05-09 21:33:56 +0600

    Minimum number of operations to make min max
    Problem:
    You are given an array A of size N.
    Let M be the minimum value present in the array initially.
    In one operation, you can choose an element Ai(1≤i≤N) and an integer X(1≤X≤100), and set Ai=X.
    Determine the minimum number of operations required to make M the maximum value in the array A.

---
+++ minimum_number_of_operations_required.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.mavenproject2;
+import java.util.*;
+public class minimum_number_of_operations_required
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for(int i=0; i<a; i++){
+		    int n = sc.nextInt();
+		    int A[] = new int[n];
+                    int m = Integer.MAX_VALUE;
+		    for(int j=0; j<n; j++){
+		        A[j] = sc.nextInt();
+                        m = Math.min(m, A[j]);
+		    }
+		    int count =0;
+		    for(int k=0; k<n; k++){
+		        if(m != A[k]){
+		            count++;
+		        }
+		    }
+		    System.out.println(count);
+		}
+	}
+}

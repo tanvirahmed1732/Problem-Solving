6a3a5a66345cc56f7ab4ba051e68c78e63a78c0d
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-05-14 16:23:32 +0600

    Squats.java
    Problem:
    
    Somu went to the gym today. He decided to do X sets of squats. Each set consists of 15 squats. Determine the total number of squats that he did today.

---
+++ Squats.java
@@ -0,0 +1,19 @@
+//Squats
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		while(n-->0){
+		    int x = sc.nextInt();
+		    System.out.println(15*x+"");
+		}
+	}
+}

e4e0ce954a9e5ccb3e642a6f4375ea54ac2a9c28
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-05-15 21:05:18 +0600

    Water Consumption.java
    Problem:
    
    Recently, Chef visited his doctor. The doctor advised Chef to drink at least 2000 ml of water each day.
    
    Chef drank X ml of water today. Determine if Chef followed the doctor's advice or not.

---
+++ Water Consumption.java
@@ -0,0 +1,22 @@
+//Water Consumption
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		while(n-->0){
+		    int x = sc.nextInt();
+		    if(x>=2000)
+		    System.out.println("YES");
+		    else
+		    System.out.println("No");
+		}
+	}
+}

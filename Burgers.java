a3df786c4300794b01b3a885e669137d8caad767
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-05-20 21:27:32 +0600

    Burgers.java
    Problem:
    
    The chef is fond of burgers and decided to make as many burgers as possible.
    
    Chef has A patties and B buns. To make 1 burger, Chef needs 1 patty and 1 bun.
    Find the maximum number of burgers that the Chef can make.

---
+++ Burgers.java
@@ -0,0 +1,22 @@
+//Burgers
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner (System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    if(a>=b)
+		    System.out.println(b);
+		    else
+		    System.out.println(a);
+		}
+	}
+}

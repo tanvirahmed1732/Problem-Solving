f4460a3d4ba93afd40d3459eb0a80b8f22a1d758
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-05-21 21:32:17 +0600

    IPL Ticket Rush.java
    Problem:
    
    DAIICT college students want to attend an IPL match.
    
    A total of N students from the college want to go while only M tickets are available for the match.
    
    Determine how many students won't be able to book tickets.

---
+++ IPL Ticket Rush.java
@@ -0,0 +1,23 @@
+//IPL Ticket Rush
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner (System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    int m = sc.nextInt();
+		    if(n-m >=0)
+		    System.out.println(n-m);
+		    else
+		    System.out.println("0");
+		}
+	}
+}

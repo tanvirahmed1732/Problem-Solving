d8b3723cc1f8edda2473ad4c1a865ea9fdc71890
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-05-23 22:36:16 +0600

    Tax in Chefland.java
    Problem:
    
    In Chefland, a tax of rupees 10 is deducted if the total income is strictly greater than rupees 100.
    
    Given that total income is X rupees, find out how much money you get.

---
+++ Tax in Chefland.java
@@ -0,0 +1,22 @@
+//Tax in Chefland
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    if(x>100)
+		    System.out.println(x-10);
+		    else
+		    System.out.println(x);
+		}
+	}
+}

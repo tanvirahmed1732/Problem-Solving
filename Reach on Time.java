aa37933934364fae498ee84e7efb08f4f6a1d966
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-05-26 00:23:32 +0600

    Reach on Time.java
    Problem:
    
    Chef has recently moved into an apartment. It takes 30 minutes for Chef to reach the office from the apartment.
    
    Chef left for the office X minutes before Chef was supposed to reach. Determine whether or not Chef will be able to reach on time.

---
+++ Reach on Time.java
@@ -0,0 +1,21 @@
+//Reach on Time
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    if(x<30)
+		    System.out.println("NO");
+		    else
+		    System.out.println("YES");
+		}
+	}
+}

032d7b0346cd959e10ea4b77bde09529af831199
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-06-02 22:02:26 +0600

    Greater Average.java
    Problem:
    
    You are given 33 numbers A, B, and C.
    
    Determine whether the average of A and B is strictly greater than C or not.
    
    NOTE: The average of A and B is defined as (A+B)/2. For example, an average of 5 and 9 is 7, an average of 5 and 8 is 6.5.

---
+++ Greater Average.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int c = sc.nextInt();
+		    double av = (a+b)/2.0;
+		    if(av>c)
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		}
+	}
+}

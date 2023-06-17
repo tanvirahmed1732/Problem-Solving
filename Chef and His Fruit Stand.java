92ee26eebde60e195633f65fe9d93f1a0e80c6e7
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-06-17 16:59:15 +0600

    Chef and His Fruit Stand.java
    Problem:
    
    Chef has closed his restaurant and decided to run a fruit stand instead. His signature dish is a fruit chaat consisting of 2 bananas and 1 apple. He currently has X bananas and Y apples. How many chaats can he make with the fruits he currently has?
    
    problem link: https://www.codechef.com/problems/FRUITCHAAT

---
+++ Chef and His Fruit Stand.java
@@ -0,0 +1,24 @@
+//Chef and His Fruit Stand
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    x = x/2;
+		    if(x<=y)
+		    System.out.println(x);
+		    else
+		    System.out.println(y);
+		}
+	}
+}

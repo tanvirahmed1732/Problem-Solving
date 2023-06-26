94bbaf94eaba6b4d316303173bb6002d2536ab41
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-06-27 01:39:15 +0600

    Is it a VOWEL or CONSONANT.java
    Problem:
    
    Write a program to take a character (C) as input and check whether the given character is a vowel or a consonant.
    
    NOTE:− Vowels are 'A', 'E', 'I', 'O', 'U'. Rest all alphabets are called consonants.
    
    problem link: https://www.codechef.com/problems/VOWELTB

---
+++ Is it a VOWEL or CONSONANT.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+		System.out.println("Vowel");
+		else
+		System.out.println("Consonant");
+	}
+}

6c8bf722742b9f1838e93b6fb03ee9b5a6aca89b
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-06-30 20:03:07 +0600

    Coldplay.java
    Problem:
    
    Chef is a big fan of Coldplay. Every Sunday, he will drive to a park taking M minutes to reach there, and during the ride he will play a single song on a loop. Today, he has got the latest song which is in total S minutes long. He is interested to know how many times will he be able to play the song completely.
    
    problem link: https://www.codechef.com/problems/SLOOP

---
+++ Coldplay.java
@@ -0,0 +1,25 @@
+/Coldplay
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int m = sc.nextInt();
+		    int s = sc.nextInt();
+		    if(m<s)
+		    System.out.println("0");
+		    else
+		    {
+		        System.out.println(m/s);
+		    }
+		}
+	}
+}

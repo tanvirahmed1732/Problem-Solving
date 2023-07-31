1bcfb459dd1e964ba089ec786225196b4f0abe98
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-07-31 17:44:13 +0600

    How much Scholarship.java
    Problem:
    
    The ZCO Scholarship Contest has just finished, and you finish with a rank of R. You know that Rank 1 to Rank 50 will get a 100% scholarship on the ZCO exam fee and Rank 51 to Rank 100 will get a 50% percentage scholarship on the ZCO exam fee. The rest do not get any scholarships.
    What percentage of the scholarship will you get?

---
+++ How much Scholarship.java
@@ -0,0 +1,21 @@
+//How much Scholarship
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		if(r<=50)
+		System.out.println("100");
+		else if(r<=100)
+		System.out.println("50");
+		else
+		System.out.println("0");
+	}
+}

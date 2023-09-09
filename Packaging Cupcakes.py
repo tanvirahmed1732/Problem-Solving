bfc86b8c50c73fbe15e871188fe5d7b4ce488d15
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-09-09 21:26:41 +0600

    Packaging Cupcakes
    Problem:
    
    Now that Chef has finished baking and frosting his cupcakes, it's time to package them. Chef has N cupcakes, and needs to decide how many cupcakes to place in each package. Each package must contain the same number of cupcakes. Chef will choose an integer A between 1 and N, inclusive, and place exactly A cupcakes into each package. Chef makes as many packages as possible. Chef then gets to eat the remaining cupcakes. Chef enjoys eating cupcakes very much. Help Chef choose the package size A that will let him eat as many cupcakes as possible.
    
    problem link: https://www.codechef.com/problems/MUFFINS3

---
+++ Packaging Cupcakes.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    n  = int(input())
+    if (n==1):
+        print(1)
+    elif(n==2):
+        print(2)
+    else:
+        print(int(n/2)+1)

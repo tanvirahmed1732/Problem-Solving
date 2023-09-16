cfa4f733a23b24ed939b334fd5461517b27e9b6a
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-09-16 22:06:52 +0600

    Elections in Chefland
    Problem:
    
    There are 101 citizens in Chefland. It is election time in Chefland and 3 parties, A, B, and C are contesting the elections. Party A receives XA​ votes, party B receives XB​ votes, and party C receives XC​ votes.
    
    The constitution of Chefland requires a particular party to receive a clear majority to form the government. A party is said to have a clear majority if it receives strictly greater than 50 votes.
    
    If any party has a clear majority, print the winning party (A, B or C). Otherwise, print NOTA.
    
    problem link: https://www.codechef.com/problems/ELECTIONS

---
+++ Elections in Chefland.py
@@ -0,0 +1,11 @@
+# Elections in Chefland
+for i in range(int(input())):
+    a , b, c = map(int, input().split())
+    if(a>50):
+        print('A')
+    elif(b>50):
+        print('B')
+    elif(c>50):
+        print('C')
+    else:
+        print('NOTA')

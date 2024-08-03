0b9b95320cb47f9b84cd724d194770dbfc924706
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2024-08-03 15:17:57 +0600

    Create Janmansh and Assignments.py

---
+++ Code Chef/Janmansh and Assignments.py
@@ -0,0 +1,8 @@
+#https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/JASSIGNMENTS
+# cook your dish here
+for _ in range(int(input())):
+    n=int(input())
+    if 10-n>=3:
+        print("Yes")
+    else:
+        print("No")

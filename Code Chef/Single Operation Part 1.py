57686dc6e48ff023cc4a6828e6fc70cc0c6c5393
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2024-11-09 17:38:39 +0600

    Create Single Operation Part 1.py

---
+++ Code Chef/Single Operation Part 1.py
@@ -0,0 +1,6 @@
+# https://www.codechef.com/problems/SINGLEOP1?tab=statement
+for _ in range(int(input())):
+    n=int(input())
+    s=input()
+    ar=list(map(str,s.split("0")))
+    print(len(ar[0]))
